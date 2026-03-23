bfd97dbb88d3e33014d705b3480b557170aba6c4
Iewnfod <iewnfoddd@outlook.com>
2026-03-23 17:20:48 +0800

    feat: init map node & path interface

---
+++ src/types/map.ts
@@ -0,0 +1,31 @@
+export interface MapNode {
+	uid: string;
+	name: string;  // 名称
+	level: string;  // 楼层
+	description?: string;
+	alias?: string;
+	accessiblePaths: string[];
+}
+
+export const MapPathTypes = {
+	Road: "road",  // 普通的路
+	Elevator: "elevator",  // 电梯
+	Escalator: "escalator",  // 扶梯
+	Stair: "stair",  // 楼梯
+}
+
+export type MapPathType = typeof MapPathTypes[keyof typeof MapPathTypes];
+
+export interface MapPath {
+	uid: string;
+	fromNodeUid: string;
+	toNodeUid: string;
+	name?: string;
+	expectPassTime: number;  // s
+	distance: number;  // m
+	type: MapPathType;
+	penalty?: number;
+	accessible?: boolean;
+	description?: string;
+	isOpenAir?: boolean;
+}
