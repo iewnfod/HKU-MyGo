f4b675c714d60edead17acc3f88317483937a0f9
Iewnfod <iewnfoddd@outlook.com>
2026-04-24 14:19:50 +0800

    feat: floating search bar list

--- src/components/FloatingSearchBar.tsx
+++ src/components/FloatingSearchBar.tsx
@@ -1,19 +1,74 @@
-import { Card, Button, InputGroup } from "@heroui/react";
+import {Card, Button, InputGroup, Separator} from "@heroui/react";
 import {SearchIcon, SettingsIcon} from "lucide-react";
+import {type ChangeEvent, useMemo, useState} from "react";
+import type {MapNode} from "@/types/map.ts";
+import {Navigator} from "@/navigator/NavCore.ts";
 
-export default function FloatingSearchBar() {
+export default function FloatingSearchBar({navigator} : {navigator: Navigator}) {
+	const [options, setOptions] = useState<MapNode[]>([]);
+	const [selectOptionIndex, setSelectOptionIndex] = useState<number>(0);
+	const shouldShowOptions = useMemo(() => options.length > 0, [options]);
+
+	const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (event.target.value !== "") {
+			setOptions((old) => {
+				return [
+					{
+						uid: crypto.randomUUID(),
+						name: event.target.value,
+						aliases: [], accessiblePaths: [],
+						level: ""
+					}
+				];
+			});
+		} else {
+			setOptions([]);
+		}
+	}
+
 	return (
 		<Card className="flex flex-col justify-center items-center min-w-24 w-full md:w-[50vw] lg:w-[40vw]" variant="transparent">
-			<div className="flex flex-col md:flex-row justify-start items-center w-full gap-4">
-				<InputGroup className="w-full">
-					<InputGroup.Input
-						className="w-full grow"
-						placeholder="Search Location..."
-					/>
-					<InputGroup.Suffix>
-						<SearchIcon className="w-4"/>
-					</InputGroup.Suffix>
-				</InputGroup>
+			<div className="flex flex-col md:flex-row justify-start items-start w-full gap-4">
+				<div className="w-full flex flex-col justify-center items-center">
+					<InputGroup className="w-full">
+						<InputGroup.Input
+							onChange={handleSearchChange}
+							className="w-full grow"
+							placeholder="Search Location..."
+						/>
+						<InputGroup.Suffix>
+							<SearchIcon className="w-4"/>
+						</InputGroup.Suffix>
+					</InputGroup>
+
+					<div className={`
+						w-full h-auto transition-all ease-in-out duration-300 overflow-hidden
+						${shouldShowOptions ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}
+					`}>
+						<Separator className="mt-3"/>
+
+						<div className="flex flex-col gap-1 w-full p-2">
+							{
+								options.map((option, index) => (
+									<Card
+										className="w-full select-none shadow-none rounded-lg"
+										variant={selectOptionIndex === index ? "secondary" : "transparent"}
+										key={index}
+									>
+										<Card.Content>
+											<div className="flex flex-col w-full">
+												<p className="text-sm">{option.name}</p>
+												<p className="text-xs text-default-500 overflow-hidden text-ellipsis text-wrap">
+													{option.description}
+												</p>
+											</div>
+										</Card.Content>
+									</Card>
+								))
+							}
+						</div>
+					</div>
+				</div>
 				<Button variant="outline" className="min-w-11 w-full md:w-auto">
 					<SettingsIcon className="text-gray-600 dark:text-gray-400"/>
 					<p className="md:hidden text-gray-600 dark:text-gray-400">Settings</p>
