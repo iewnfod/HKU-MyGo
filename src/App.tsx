54821d0784413a4fb5b5a687e0022858649cc74a
Iewnfod <iewnfoddd@outlook.com>
2026-04-24 14:19:50 +0800

    feat: floating search bar list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import FloatingSearchBar from "./components/FloatingSearchBar.tsx";
+import FloatingSearchBar from "@/components/FloatingSearchBar.tsx";
+import {useState} from "react";
+import {Navigator} from "@/navigator/NavCore.ts";
+import nodeJson from "@/../data/nodes.json";
+import pathJson from "@/../data/paths.json";
 
 function App() {
+	const [navigator, setNavigator] = useState<Navigator>(
+		// @ts-expect-error json type is manually maintained
+		new Navigator(nodeJson, pathJson)
+	);
+
 	return (
 		<div className="w-screen h-screen overflow-hidden p-4">
-			<FloatingSearchBar/>
+			<FloatingSearchBar navigator={navigator}/>
 		</div>
 	);
 }
