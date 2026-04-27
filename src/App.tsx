2f124ce23e2fc6e0c8501fc557bfc351c735acb2
Iewnfod <iewnfoddd@outlook.com>
2026-04-27 11:24:02 +0800

    fix: some overflow and illogical layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import FloatingSearchBar from "@/components/FloatingSearchBar.tsx";
-import {Navigator} from "@/navigator/NavCore.ts";
+import {Navigator, RoutingMode} from "@/navigator/NavCore.ts";
 import {sanitizeMapData} from "@/navigator/DataSanitizer.ts";
 import nodeJson from "@/../data/nodes.json";
 import pathJson from "@/../data/paths.json";
+import {useMemo, useState} from "react";
+import type {MapNode, MapPath} from "@/types/map.ts";
+import RouteStepList from "@/components/RouteStepList.tsx";
 
 function App() {
 	const navigator = new Navigator(sanitizeMapData(nodeJson.nodes, pathJson.paths));
+	const [segments, setSegments] = useState<MapPath[]>([]);
+	const [totalTime, setTotalTime] = useState<number>(0);
+	const [totalDistance, setTotalDistance] = useState<number>(0);
+	const hasResult = useMemo(() => segments.length > 0, [segments]);
+
+	const handleGeneratePath = (start: MapNode, end: MapNode) => {
+		const routeResult = navigator.findPath(start.uid, end.uid, RoutingMode.FastestNormal);
+		console.log(routeResult);
+		if (routeResult) {
+			const pathSegments = navigator.findPathSegments(routeResult.path, RoutingMode.FastestNormal);
+			console.log(pathSegments);
+			if (pathSegments) {
+				setSegments(pathSegments);
+				setTotalTime(routeResult.totalTime);
+				setTotalDistance(routeResult.totalDistance);
+			}
+		}
+	}
 
 	return (
-		<div className="w-screen h-screen overflow-hidden flex flex-row justify-between items-center p-4">
-			<FloatingSearchBar navigator={navigator}/>
+		<div className="w-screen h-screen overflow-hidden flex flex-col justify-start lg:flex-row lg:justify-between items-center p-4 min-w-96 overflow-y-auto">
+			<FloatingSearchBar navigator={navigator} onGeneratePath={handleGeneratePath} hasResult={hasResult}/>
+			<div className={`w-full grow lg:max-w-[55vw] p-4 pt-0 h-auto max-h-full lg:max-h-screen ${hasResult ? 'h-full' : ''}`}>
+				{hasResult && (
+					<RouteStepList totalTime={totalTime} totalDistance={totalDistance} segments={segments} />
+				)}
+			</div>
 		</div>
 	);
 }
--- src/components/FloatingSearchBar.tsx
+++ src/components/FloatingSearchBar.tsx
@@ -1,57 +1,42 @@
 import {Card, Button} from "@heroui/react";
 import {SettingsIcon} from "lucide-react";
-import {Navigator, RoutingMode} from "@/navigator/NavCore.ts";
+import {Navigator} from "@/navigator/NavCore.ts";
 import SearchInput from "@/components/SearchInput.tsx";
-import RouteStepList from "@/components/RouteStepList.tsx";
-import {useState} from "react";
-import type {MapNode, MapPath} from "@/types/map.ts";
+import {useCallback, useState} from "react";
+import type {MapNode} from "@/types/map.ts";
 
-export default function FloatingSearchBar({navigator} : {navigator: Navigator}) {
+export default function FloatingSearchBar({navigator, onGeneratePath, hasResult} : {
+	navigator: Navigator;
+	onGeneratePath: (start: MapNode, end: MapNode) => void;
+	hasResult: boolean;
+}) {
 	const [start, setStart] = useState<MapNode | null>(null);
 	const [end, setEnd] = useState<MapNode | null>(null);
-	const [segments, setSegments] = useState<MapPath[]>([]);
-	const [totalTime, setTotalTime] = useState<number>(0);
-	const [totalDistance, setTotalDistance] = useState<number>(0);
 
-	const handleGeneratePath = () => {
+	const handleGeneratePath = useCallback(() => {
 		if (start === null || end === null) return;
-		const routeResult = navigator.findPath(start.uid, end.uid, RoutingMode.FastestNormal);
-		console.log(routeResult);
-		if (routeResult) {
-			const pathSegments = navigator.findPathSegments(routeResult.path, RoutingMode.FastestNormal);
-			console.log(pathSegments);
-			if (pathSegments) {
-				setSegments(pathSegments);
-				setTotalTime(routeResult.totalTime);
-				setTotalDistance(routeResult.totalDistance);
-			}
-		}
-	}
+		onGeneratePath(start, end);
+	}, [start, end, onGeneratePath]);
 
 	return (
-		<Card className="flex flex-col justify-start items-center min-w-24 h-full gap-4" variant="transparent">
+		<Card className={`flex flex-col justify-start items-center w-full min-w-24 ${hasResult ? '' : 'h-full'} lg:h-full lg:grow gap-4 min-h-90`} variant="transparent">
 			<div className="flex flex-row justify-between items-center w-full">
 				<img
 					src="/logo.svg"
 					alt="HKU | My Go"
 				/>
-				<Button variant="outline" className="min-w-11 w-full md:w-auto">
+				<Button variant="outline" className="min-w-11 lg:w-auto">
 					<SettingsIcon className="text-gray-600 dark:text-gray-400"/>
 					<p className="text-gray-600 dark:text-gray-400">Settings</p>
 				</Button>
 			</div>
-			<div className="flex flex-col md:flex-row justify-between items-start gap-4">
-				<div className="flex flex-col gap-4 items-start justify-start">
-					<SearchInput navigator={navigator} label="Start" onSelect={setStart} />
-					<SearchInput navigator={navigator} label="End" onSelect={setEnd} />
-				</div>
+			<div className="flex flex-col gap-4 items-start justify-start w-full">
+				<SearchInput navigator={navigator} label="Start" onSelect={setStart} />
+				<SearchInput navigator={navigator} label="End" onSelect={setEnd} />
 			</div>
 			<Button className="w-full mt-2" isDisabled={start === null || end === null || start === end} onClick={handleGeneratePath}>
 				Generate Path!
 			</Button>
-			{segments.length > 0 && (
-				<RouteStepList totalTime={totalTime} totalDistance={totalDistance} segments={segments} />
-			)}
 		</Card>
 	);
 }
